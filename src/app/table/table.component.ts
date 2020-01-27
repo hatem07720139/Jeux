bbee237d29aa4d8ca5cd6d929fd753581aad6201
hatem07720139 <hatemshilihatem1234@gmail.com>
2020-01-27 16:28:00 +0100

    fonction exuxuter player

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
- 
+  // public typeplay="";
+  // public n=0;
+  player = 'X';
+  game = ['','','','','','','','','',]
+  players(index){
+    this.game[index] = this.player;
+    this.player = this.player === 'O' ? 'X' : 'O';
+    console.log(this.game)
+    // return;
+    // for (let i = 0; i < 9; i++) {
+    //   if (this.n%2===0) {
+    //     this.typeplay="X";
+    //     this.n=this.n+1;
+    //   }else{
+    //     this.typeplay="O";
+    //     this.n=this.n+1;
+    //   }
+    // }
+    // console.log(n);
+  }
   constructor() { }
 
   ngOnInit() {
