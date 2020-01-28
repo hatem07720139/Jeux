4e2fcd098a55a7d8cab1689ed89db8509ad8b86c
hatem07720139 <hatemshilihatem1234@gmail.com>
2020-01-28 02:01:01 +0100

    Winner Player

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -6,29 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-  // public typeplay="";
-  // public n=0;
+  winner: boolean;
   player = 'X';
-  game = ['','','','','','','','','',]
-  players(index){
-    this.game[index] = this.player;
-    this.player = this.player === 'O' ? 'X' : 'O';
-    console.log(this.game)
-    // return;
-    // for (let i = 0; i < 9; i++) {
-    //   if (this.n%2===0) {
-    //     this.typeplay="X";
-    //     this.n=this.n+1;
-    //   }else{
-    //     this.typeplay="O";
-    //     this.n=this.n+1;
-    //   }
-    // }
-    // console.log(n);
+  game = ['', '', '', '', '', '', '', '', '',]
+  players(index) {
+    if (this.game[index] === '') {
+      this.game[index] = this.player;
+      this.player = this.player === 'O' ? 'X' : 'O';
+    }
   }
+  Thewinner(playX, playO) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [2, 5, 8],
+      [1, 4, 7],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      this.winner = false;
+      if (this.game[a] && this.game[a] === this.game[b] && this.game[a] === this.game[c]) {
+        alert("the Winner is Player " + this.game[a])
+        if (this.game[a] === "X") {
+          playX.value = Number(playX.value) + 1;
+        }
+        else {
+          playO.value = Number(playO.value) + 1;
+        }
+        this.winner = true;
+        break;
+      }
+    }
+    for (let i = 0; i < this.game.length; i++) {
+      if (this.winner === true) {
+        this.game[i] = "";
+      }
+    }
+  }
+
+
   constructor() { }
 
   ngOnInit() {
   }
-  
+
 }
